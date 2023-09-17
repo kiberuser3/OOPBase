7a630d0f14325d0a969ad142f7d178e79b7dd95d
DimaPuz2010 <91826731+DimaPuz2010@users.noreply.github.com>
2023-09-17 12:54:47 +0600

    OOPb

---
+++ OOPBase/src/main/kotlin/Main.kt
@@ -0,0 +1,67 @@
+fun main(args: Array<String>) {
+
+}
+
+// Задачи по теме if-else
+fun ifFirstTask() {
+    var x = 6
+    var y = 8
+
+    if (y != 0) {
+        println(x / y)
+    } else {
+        println("Делить на ноль нельзя")
+    }
+
+    println("Прогамма закончена")
+}
+
+fun ifSecondTask() {
+    var p = 4
+    var b = 6
+    var fg = p + b
+    println("")
+
+}
+
+fun ifThirdTask() {
+    val month = 12
+    if (month == 12) {
+        println("Декабрь")
+    }
+    else if (month == 11) {
+        println("Ноябрь")
+    }
+    else if (month == 10) {
+        println("Октябрь")
+    }
+    else if (month == 9) {
+        println("Сентябрь")
+    }
+    else if (month == 8) {
+        println("Август")
+    }
+    else if (month == 7) {
+        println("Июнь")
+    }
+    else if (month == 6) {
+        println("Июль")
+    }
+    else if (month == 5) {
+        println("Май")
+    }
+    else if (month == 4) {
+        println("Апрель")
+    }
+    else if (month == 3) {
+        println("Март")
+    }
+    else if (month == 2) {
+        println("Февраль")
+    }
+    else if (month == 1) {
+        println("Январь")
+    } else {
+
+    }
+}
